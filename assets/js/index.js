ac3d942f76c015b0fa7f510e4a52cde50c8df29b
Gabriel Luan <gabrielassembleiano@gmail.com>
2025-01-13 20:31:00 -0300

    finalizando o personagem vegeta

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@
 const btnPositive = document.querySelector(".btn1");
 const btnNegative = document.querySelector(".btn2");
 const reset = document.getElementById("reset");
-const personagem = document.querySelector("img");
+const personagem = document.querySelector("#guerreiro");
 const level = document.getElementById("lvl");
 const strenght = document.getElementById("str");
 let h2 = document.querySelector("h2");
@@ -35,17 +35,17 @@ function LevelUp() {
         personagem.style.width = "200px";
     } else if (count === 29) {
         personagem.setAttribute("src", "./assets/img/goku-4.png");
-        h2.style.color = "yellow";
+        h2.style.color = "rgb(191, 202, 27)";
         h2.textContent = "Son Goku (Ssj)"
         personagem.style.width = "200px"
     } else if (count === 39) {
         personagem.setAttribute("src", "./assets/img/goku-5.png");
-        h2.style.color = "yellow";
+        h2.style.color = "rgb(191, 202, 27)";
         h2.textContent = "Son Goku (Ssj2)"
         personagem.style.width = "200px"
     } else if (count === 49) {
         personagem.setAttribute("src", "./assets/img/goku-7.png");
-        h2.style.color = "yellow";
+        h2.style.color = "rgb(191, 202, 27)";
         h2.textContent = "Son Goku (Ssj3)"
         personagem.style.width = "200px"
     } else if (count === 59) {
@@ -105,87 +105,74 @@ function RemoveLevel() {
     const addStrength = () => {
         strenght.textContent = count * 80;
     }
-    if (count <= 15) {
-        personagem.setAttribute("src", "./assets/img/goku-1.png");
-        h2.style.color = "#000"
-        h2.textContent = "Son Goku (Criança)"
-        personagem.style.width = "150px"
-    } else if (count <= 25) {
-        personagem.setAttribute("src", "./assets/img/goku-2.png");
-        h2.style.color = "#000"
-        h2.textContent = "Son Goku (Adulto)"
-        personagem.style.width = "150px"
-    } else if (count <= 30) {
-        personagem.setAttribute("src", "./assets/img/goku-3.png");
-        h2.style.color = "rgb(70, 5, 5)";
-        h2.textContent = "Son Goku (Kaioken)"
-        personagem.style.width = "200px"
-    } else if (count <= 40) {
-        personagem.setAttribute("src", "./assets/img/goku-4.png");
-        h2.style.color = "yellow";
-        h2.textContent = "Son Goku (Ssj)"
-        personagem.style.width = "200px"
-    } else if (count <= 50) {
-        personagem.setAttribute("src", "./assets/img/goku-5.png");
-        h2.style.color = "yellow";
-        h2.textContent = "Son Goku (Ssj2)"
-        personagem.style.width = "200px"
-    } else if (count <= 60) {
-        personagem.setAttribute("src", "./assets/img/goku-7.png");
-        h2.style.color = "yellow";
-        h2.textContent = "Son Goku (Ssj3)"
-        personagem.style.width = "200px"
-    } else if (count <= 70) {
-        personagem.setAttribute("src", "./assets/img/goku-7.4.png");
-        h2.style.color = "red";
-        h2.textContent = "Son Goku (Ssj-god)"
-        personagem.style.width = "200px"
-    } else if (count <= 80) {
-        personagem.setAttribute("src", "./assets/img/goku-8.png");
-        h2.style.color = "blue";
-        h2.textContent = "Son Goku (Ssj-blue)"
-        personagem.style.width = "270px"
-    } else if (count <= 90) {
-        personagem.setAttribute("src", "./assets/img/goku-7.5.png");
-        h2.style.color = "red";
-        h2.textContent = "Son Goku (Ssj-blue kaioken)"
-        personagem.style.width = "200px"
-    } else if (count <= 100) {
-        personagem.setAttribute("src", "./assets/img/goku-11.png");
-        h2.style.color = "blue";
-        h2.textContent = "Son Goku (Instinto Superior)"
-        personagem.style.width = "200px"
-    } else if (count <= 120) {
-        personagem.setAttribute("src", "./assets/img/goku-11.png");
-        h2.style.color = "blue";
-        h2.textContent = "Son Goku (Instinto Superior)"
-        personagem.style.width = "200px"
-    } else if (count <= 140) {
-        personagem.setAttribute("src", "./assets/img/goku-12.png");
-        h2.style.color = "blue";
-        h2.textContent = "Son Goku (Instinto Superior)"
-        personagem.style.width = "200px"
-    } else if (count <= 150) {
-        personagem.setAttribute("src", "./assets/img/goku-13.png");
-        h2.style.color = "#ccc";
-        h2.textContent = "Son Goku (Instinto Superior)"
-        personagem.style.width = "200px"
-    }
+    // if (count <= 15) {
+    //     personagem.setAttribute("src", "./assets/img/goku-1.png");
+    //     h2.style.color = "#000"
+    //     h2.textContent = "Son Goku (Criança)"
+    //     personagem.style.width = "150px"
+    // } else if (count <= 25) {
+    //     personagem.setAttribute("src", "./assets/img/goku-2.png");
+    //     h2.style.color = "#000"
+    //     h2.textContent = "Son Goku (Adulto)"
+    //     personagem.style.width = "150px"
+    // } else if (count <= 30) {
+    //     personagem.setAttribute("src", "./assets/img/goku-3.png");
+    //     h2.style.color = "rgb(70, 5, 5)";
+    //     h2.textContent = "Son Goku (Kaioken)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 40) {
+    //     personagem.setAttribute("src", "./assets/img/goku-4.png");
+    //     h2.style.color = "yellow";
+    //     h2.textContent = "Son Goku (Ssj)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 50) {
+    //     personagem.setAttribute("src", "./assets/img/goku-5.png");
+    //     h2.style.color = "yellow";
+    //     h2.textContent = "Son Goku (Ssj2)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 60) {
+    //     personagem.setAttribute("src", "./assets/img/goku-7.png");
+    //     h2.style.color = "yellow";
+    //     h2.textContent = "Son Goku (Ssj3)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 70) {
+    //     personagem.setAttribute("src", "./assets/img/goku-7.4.png");
+    //     h2.style.color = "red";
+    //     h2.textContent = "Son Goku (Ssj-god)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 80) {
+    //     personagem.setAttribute("src", "./assets/img/goku-8.png");
+    //     h2.style.color = "blue";
+    //     h2.textContent = "Son Goku (Ssj-blue)"
+    //     personagem.style.width = "270px"
+    // } else if (count <= 90) {
+    //     personagem.setAttribute("src", "./assets/img/goku-7.5.png");
+    //     h2.style.color = "red";
+    //     h2.textContent = "Son Goku (Ssj-blue kaioken)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 100) {
+    //     personagem.setAttribute("src", "./assets/img/goku-11.png");
+    //     h2.style.color = "blue";
+    //     h2.textContent = "Son Goku (Instinto Superior)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 120) {
+    //     personagem.setAttribute("src", "./assets/img/goku-11.png");
+    //     h2.style.color = "blue";
+    //     h2.textContent = "Son Goku (Instinto Superior)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 140) {
+    //     personagem.setAttribute("src", "./assets/img/goku-12.png");
+    //     h2.style.color = "blue";
+    //     h2.textContent = "Son Goku (Instinto Superior)"
+    //     personagem.style.width = "200px"
+    // } else if (count <= 150) {
+    //     personagem.setAttribute("src", "./assets/img/goku-13.png");
+    //     h2.style.color = "#ccc";
+    //     h2.textContent = "Son Goku (Instinto Superior)"
+    //     personagem.style.width = "200px"
+    // }
     AddLevel();
     addStrength();
-}
-
-//Função para resetar o nivel e voltar ao nivel 0
-function ResetLevel() {
-    // Redefine o valor do contador para 0
-    count = 0;
-
-    // Atualiza os elementos na interface
-    h2.style.color = "#000"
-    h2.textContent = "Son Goku (Criança)"
-    level.innerText = "0";
-    strenght.innerText = "0";
-    personagem.setAttribute("src", "./assets/img/goku-1.png");
 }
 
 //Eventos que chamam as suas devidas funções
--- assets/js/vegeta.js
+++ assets/js/vegeta.js
@@ -2,13 +2,13 @@
 const btnPositive = document.querySelector(".btn1");
 const btnNegative = document.querySelector(".btn2");
 const reset = document.getElementById("reset");
-const personagem = document.querySelector("img");
+const personagem = document.querySelector("#guerreiro");
 const level = document.getElementById("lvl");
 const strenght = document.getElementById("str");
 let h2 = document.querySelector("h2");
 
 //variavel que armazena o número do nivel
-let count = 0;
+let count =0;
 
 
 function LevelUp() {
@@ -34,64 +34,66 @@ function LevelUp() {
         h2.textContent = "Vegeta (Ssj)";
         personagem.style.width = "200px";
     } else if (count === 29) {
+        personagem.setAttribute("src", "./assets/img/vegeta-img/vegeta-majin.png");
+        h2.style.color = "rgb(191, 202, 27)";
+        h2.textContent = "Vegeta (Ssj majin)"
+        personagem.style.width = "200px"
+    } else if (count === 49) {
         personagem.setAttribute("src", "/assets/img/vegeta-img/vegeta-ssj2.jpg");
         h2.style.color = "rgb(191, 202, 27)";
         h2.textContent = "Vegeta (Ssj2)"
         personagem.style.width = "200px"
-    } else if (count === 39) {
-        personagem.setAttribute("src", "./assets/img/goku-5.png");
-        h2.style.color = "yellow";
-        h2.textContent = "Son Goku (Ssj2)"
-        personagem.style.width = "200px"
-    } else if (count === 49) {
-        personagem.setAttribute("src", "./assets/img/goku-7.png");
-        h2.style.color = "yellow";
-        h2.textContent = "Son Goku (Ssj3)"
-        personagem.style.width = "200px"
+
     } else if (count === 59) {
-        personagem.setAttribute("src", "./assets/img/goku-7.4.png");
+        personagem.setAttribute("src", "/assets/img/vegeta-img/vegeta-god.png");
         h2.style.color = "red";
-        h2.textContent = "Son Goku (Ssj-god)"
-        personagem.style.width = "200px"
-    } else if (count === 69) {
-        personagem.setAttribute("src", "./assets/img/goku-8.png");
+        h2.textContent = "Vegeta (Ssj-god)"
+        personagem.style.width = "150px"
+    } else if (count === 79) {
+        personagem.setAttribute("src", "/assets/img/vegeta-img/vegeta-blue.png");
         h2.style.color = "blue";
-        h2.textContent = "Son Goku (Ssj-blue)"
-        personagem.style.width = "270px"
-    }
-    else if (count === 79) {
-        personagem.setAttribute("src", "./assets/img/goku-7.5.png");
-        h2.style.color = "red";
-        h2.textContent = "Son Goku (Ssj-blue kaioken)"
-        personagem.style.width = "200px"
+        h2.textContent = "Vegeta (Ssj-blue)"
+        personagem.style.width = "180px"
     } else if (count === 99) {
-        personagem.setAttribute("src", "./assets/img/goku-11.png");
+        personagem.setAttribute("src", "/assets/img/vegeta-img/vegeta-blue-ev.png");
         h2.style.color = "blue";
-        h2.textContent = "Son Goku (Instinto Superior)"
+        h2.textContent = "Vegeta (ISsj-blue evolution)"
         personagem.style.width = "200px"
     }
     else if (count === 119) {
-        personagem.setAttribute("src", "./assets/img/goku-12.png");
-        h2.style.color = "blue";
-        h2.textContent = "Son Goku (Instinto Superior)"
-        personagem.style.width = "200px"
-    } else if (count === 139) {
-        personagem.setAttribute("src", "./assets/img/goku-13.png");
-        h2.style.color = "#999";
-        h2.textContent = "Son Goku (Instinto Superior)"
+        personagem.setAttribute("src", "/assets/img/vegeta-img/vegeta-ego.png");
+        h2.style.color = "purple";
+        h2.textContent = "Vegeta (Ultra Ego)"
         personagem.style.width = "200px"
     }
-    else if (count === 149) {
-        personagem.setAttribute("src", "./assets/img/goku-14.png");
-        h2.style.color = "#ccc";
-        h2.textContent = "Son Goku (Instinto Superior Perfeito)"
-        personagem.style.width = "200px"
+    else if (count >= 149) {
+        personagem.setAttribute("src", "/assets/img/vegeta-img/vegeta-ultra-ego.png");
+        h2.style.color = "purple";
+        h2.textContent = "Vegeta (Ultra Ego)"
+        personagem.style.width = "250px"
+    }
+    AddLevel();
+    addStrength();
+} function RemoveLevel() {
+    const AddLevel = () => {
+        level.textContent = count - 1;
+        count--;
+        if (count <= 0) {
+            level.textContent = 0;
+            strenght.textContent = 0;
+            strenght.innerText = "0";
+        }
+    }
+    const addStrength = () => {
+        strenght.textContent = count * 80;
     }
     AddLevel();
     addStrength();
 }
 
+
 btnPositive.addEventListener("click", LevelUp);
+btnNegative.addEventListener("click", RemoveLevel);
 
 function trocarPersonagem() {
     const select = document.getElementById("escolha-personagem");
